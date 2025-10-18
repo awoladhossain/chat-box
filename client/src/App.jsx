ac219f41d396d1b70f16e740f0a1f7add7d874dc
Awolad Hossain <awoladh04@gmail.com>
2025-10-19 00:52:16 +0600

    initial setup for frontend all the dependecy added

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello bhai</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
